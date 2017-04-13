599bf6a22b708d4a298a95d34856dcb9c3eb124e
Virginiacxy <906033130@qq.com>
2017-04-12 19:20:41 -0700

    exercise6

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,20 +2,21 @@
 
 # Create a vector of the number of points the Seahawks scored the first 4 games of last season
 # Hint: (google "Seahawks scores 2016")
-
+points <- c(31,25,20,6)
 # Create a vector of the number of points the Seahawks have allowed to be scored against them in the first 4 games
-
+points.against <- c(24,31,25,6)
 
 # Combine your two vectors into a dataframe
-
+my.data <- data.frame(points, points.against)
 
 # Create a new column "diff" that is the difference in points
-
+my.data$diff <- points - points.against
 
 # Create a new column "won" which is TRUE if the Seahawks wom
-
+my.data$won <- points > points.against
 
 # Create a vector of the opponents
-
+opponents <- c('Patriots', 'Bills', 'Saints', 'Cardinals')
 
 # Assign your dataframe rownames of their opponents
+my.data$opponents <- opponents
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,25 +1,25 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
-
-
+employees <- c(paste('Employee', 1:100))
+paste('a','b', sep="")
 # Create a vector of 2014 salaries using the runif function
-
+salaries.2014 <- runif(employees, min = 1000, max = 10000)
 
 # Create a vector of 2015 salaries that are typically higher than the 2014 salaires (use runif again)
-
+salaries.2015 <- runif(employees, min = 3000, max = 30000)
 
 # Create a data.frame 'salaries' by combining the vectors you just made
-
+salaries <- data.frame(employees, salaries.2014, salaries.2015)
 
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
+salaries$raise <- salaries.2015 - salaries.2014
 
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
+salaries$got.raise <- salaries.2015 > salaries.2014
 
 # Retrieve values from your data frame to answer the following questions:
 
 # What was the 2015 salary of employee 57
-
+salaries.2015[57]
 
 # How many employees got a raise?
 
@@ -54,3 +54,6 @@
 # Repeat the above experiment 100 times, tracking the loss each time.
 # Does the average loss equal what you expect?
 # What about 10,000 times?
+today <- as.Date('2017-04-6')
+next.thursday <- as.Date('2017-04-13')
+next.thursday - today
